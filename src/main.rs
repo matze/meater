4a5793c4bac9d31d5f16e64d040f6d4f8f517b33
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2025-01-06 22:10:17 +0100

    Use stream-based approach to consolidate state

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,72 @@ use embedded_graphics::mono_font::MonoTextStyle;
 use embedded_graphics::pixelcolor::BinaryColor;
 use embedded_graphics::text::Text;
 use embedded_graphics::Drawable;
+use futures::{stream, Stream, StreamExt};
 use profont::{PROFONT_24_POINT, PROFONT_9_POINT};
+use tokio::sync::mpsc;
 
 mod meater;
+
+/// Consolidated MEATER state
+#[derive(Clone, Debug)]
+struct MeaterState {
+    tip: f32,
+    ambient: f32,
+    percentage: u16,
+}
+
+/// Consolidate events
+enum Event {
+    Disconnected,
+    Connecting,
+    Update(MeaterState),
+}
+
+/// Turn [`meater::Event`]s into consolidate state [`Event`]s.
+fn process_events(receiver: mpsc::Receiver<meater::Event>) -> impl Stream<Item = Event> {
+    struct State {
+        receiver: mpsc::Receiver<meater::Event>,
+        state: MeaterState,
+    }
+
+    let stream_state = State {
+        receiver,
+        state: MeaterState {
+            tip: 20.0,
+            ambient: 20.0,
+            percentage: 100,
+        },
+    };
+
+    stream::unfold(stream_state, |mut stream_state| async move {
+        loop {
+            if let Some(event) = stream_state.receiver.recv().await {
+                match event {
+                    meater::Event::State(state) => match state {
+                        meater::State::Disconnected => {
+                            break Some((Event::Disconnected, stream_state))
+                        }
+                        meater::State::Connecting => break Some((Event::Connecting, stream_state)),
+                        meater::State::Connected => {
+                            // TODO: send out an event that causes a different icon to be shown
+                        }
+                    },
+                    meater::Event::Temperature { tip, ambient } => {
+                        stream_state.state.tip = tip;
+                        stream_state.state.ambient = ambient;
+                        break Some((Event::Update(stream_state.state.clone()), stream_state));
+                    }
+                    meater::Event::Battery { percent } => {
+                        stream_state.state.percentage = percent;
+                        break Some((Event::Update(stream_state.state.clone()), stream_state));
+                    }
+                }
+            } else {
+                break None;
+            }
+        }
+    })
+}
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> anyhow::Result<()> {
@@ -46,55 +109,41 @@ async fn main() -> anyhow::Result<()> {
 
     let temperature_style = MonoTextStyle::new(&PROFONT_24_POINT, BinaryColor::On);
     let description_style = MonoTextStyle::new(&PROFONT_9_POINT, BinaryColor::On);
-    let mut tip_temperature = 20.0;
-    let mut ambient_temperature = 20.0;
-    let mut battery_percentage = 100;
 
-    let (client, mut receiver) = meater::Client::new();
+    let (client, receiver) = meater::Client::new();
 
     let event_handling = async move {
-        let mut state = meater::State::Disconnected;
+        let mut stream = std::pin::pin!(process_events(receiver));
 
-        while let Some(event) = receiver.recv().await {
-            match event {
-                meater::Event::State(new_state) => state = new_state,
-                meater::Event::Temperature { tip, ambient } => {
-                    tip_temperature = tip;
-                    ambient_temperature = ambient;
-                }
-                meater::Event::Battery { percent } => {
-                    battery_percentage = percent;
-                }
-            }
-
+        while let Some(event) = stream.next().await {
             display.clear();
 
-            match state {
-                meater::State::Disconnected => {
+            match event {
+                Event::Disconnected => {
                     Image::new(&not_found_icon, Point::new(47, 16))
                         .draw(&mut display)
                         .unwrap();
                 }
-                meater::State::Connecting => {
+                Event::Connecting => {
                     Image::new(&connecting_icon, Point::new(47, 16))
                         .draw(&mut display)
                         .unwrap();
                 }
-                meater::State::Connected => {
-                    Text::new(
-                        &format!("{tip_temperature:.0}"),
-                        Point::new(0, 28),
-                        temperature_style,
-                    )
-                    .draw(&mut display)
-                    .unwrap();
+                Event::Update(MeaterState {
+                    tip,
+                    ambient,
+                    percentage,
+                }) => {
+                    Text::new(&format!("{tip:.0}"), Point::new(0, 28), temperature_style)
+                        .draw(&mut display)
+                        .unwrap();
 
                     Text::new(&format!("tip"), Point::new(34, 27), description_style)
                         .draw(&mut display)
                         .unwrap();
 
                     Text::new(
-                        &format!("{ambient_temperature:.0}"),
+                        &format!("{ambient:.0}"),
                         Point::new(0, 60),
                         temperature_style,
                     )
@@ -105,7 +154,7 @@ async fn main() -> anyhow::Result<()> {
                         .draw(&mut display)
                         .unwrap();
 
-                    let icon = match battery_percentage {
+                    let icon = match percentage {
                         ..=25 => battery_icon_25,
                         26..=50 => battery_icon_50,
                         51..=75 => battery_icon_75,
